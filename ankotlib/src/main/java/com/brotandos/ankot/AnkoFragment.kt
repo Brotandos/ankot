6d627d3d54a433510ba48385e4e89c2d9b6ede6e
Brotandos <brotandos@gmail.com>
2018-03-14 16:36:13 +0600

    0.0.Y-fix: AnkoFragment fix

--- ankotlib/src/main/java/com/brotandos/ankot/AnkoFragment.kt
+++ ankotlib/src/main/java/com/brotandos/ankot/AnkoFragment.kt
@@ -3,6 +3,7 @@ package com.brotandos.ankot
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.dip
@@ -13,9 +14,11 @@ import org.jetbrains.anko.support.v4.UI
  * @date: 14.03.2018.
  */
 abstract class AnkoFragment : Fragment() {
-    abstract fun ui(): AnkoContext<Fragment>.() -> Unit
+    fun Fragment.KUI(init: AnkoContext<Fragment>.() -> Unit) = UI(init).view
 
-    override fun onCreateView(i: LayoutInflater, vg: ViewGroup?, b: Bundle?) = UI { ui() }.view
+    abstract fun markup(): View
+
+    override fun onCreateView(i: LayoutInflater, vg: ViewGroup?, b: Bundle?) = markup()
 
     inline val Int.dp: Int get() = context!!.dip(this)
 }
